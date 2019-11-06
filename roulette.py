d595a082cb2c026afb99ff569da12cce10396d16
Justin Gluska <justingluska@gmail.com>
2019-11-06 12:22:29 -0500

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -3,12 +3,16 @@
 # ROULETTE GAME
 
 from graphics import *
-
+import math
 win = GraphWin("Roulette Wheel", 1000, 1000)
 win.setCoords(-90, -90, 90, 90)
 aCircle = Circle(Point(0,0), 90)
 aCircle.draw(win)
-aLine = Line(Point(0,180), Point(0,-180))
-aLine.draw(win)
-aLine = Line(Point(9.47,180-9.47), Point(-9.47*2,-180+(9.47*2)))
-aLine.draw(win)
+#aLine = Line(Point(0,0), Point(90*math.cos(0),90*math.sin(0)))
+#aLine.draw(win)
+#aLine = Line(Point(0,0), Point(90*math.cos(0.523599),90*math.sin(0.523599)))
+#aLine.draw(win)
+for x in range(38):
+    temp = (math.pi*x)/180
+    aLine = Line(Point(0,0), Point(90*math.cos(temp),90*math.sin(temp)))
+    aLine.draw(win)
