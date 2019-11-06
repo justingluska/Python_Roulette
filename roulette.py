d3a73db9abda19dbdd632a5b0b938a6ce52d3824
Justin Gluska <justingluska@gmail.com>
2019-11-06 16:24:39 -0500

    wheel colored

--- roulette.py
+++ roulette.py
@@ -2,19 +2,26 @@
 # ECS102 FINAL PROJECT
 # ROULETTE GAME
 
+### IMPORT GRAPHICS AND MATH
 from graphics import *
 import math
-wheel = GraphWin("Roulette Wheel", 1000, 1000)
-wheel.setBackground("lightblue")
-otherWin = GraphWin("Bet Controls", 600, 300)
+### SET WHEEL WINDOW CONDITIONS
+wheel = GraphWin("Roulette Wheel", 650, 650)
 wheel.setCoords(-90, -90, 90, 90)
-aCircle = Circle(Point(0,0), 90)
-aCircle.draw(wheel)
+wheel.setBackground("lightblue")
+#################################
+### CREATE ROULETTE WHEEL
+rouletteCircle = Circle(Point(0,0), 90)
+rouletteCircle.draw(wheel)
 for x in range(38):
     temp = (math.pi*x)/180
-    aLine = Line(Point(0,0), Point(90*math.cos(temp*9.4736842105),90*math.sin(temp*9.4736842105)))
+    temp2 = (math.pi*(x+1))/180
+    aLine = Polygon(Point(0,0), Point(90*math.cos(temp*9.4736842105),90*math.sin(temp*9.4736842105)), Point(90*math.cos(temp2*9.4736842105),90*math.sin(temp2*9.4736842105)))
     if x%2 == 0:
         aLine.setFill("red")
     elif x%2 == 1:
         aLine.setFill("black")
     aLine.draw(wheel)
+### CREATE CONTROL WINDOW
+otherWin = GraphWin("Bet Controls", 600, 300)
+otherWin.setBackground("grey")
