c2ac27c707b286386dba7f4f99b105c86b8afbe4
Justin Gluska <justingluska@gmail.com>
2019-11-06 12:31:36 -0500

    added second wheel

--- roulette.py
+++ roulette.py
@@ -4,15 +4,17 @@
 
 from graphics import *
 import math
-win = GraphWin("Roulette Wheel", 1000, 1000)
-win.setCoords(-90, -90, 90, 90)
+wheel = GraphWin("Roulette Wheel", 1000, 1000)
+wheel.setBackground("lightblue")
+otherWin = GraphWin("Bet Controls", 600, 300)
+wheel.setCoords(-90, -90, 90, 90)
 aCircle = Circle(Point(0,0), 90)
-aCircle.draw(win)
-#aLine = Line(Point(0,0), Point(90*math.cos(0),90*math.sin(0)))
-#aLine.draw(win)
-#aLine = Line(Point(0,0), Point(90*math.cos(0.523599),90*math.sin(0.523599)))
-#aLine.draw(win)
+aCircle.draw(wheel)
 for x in range(38):
     temp = (math.pi*x)/180
     aLine = Line(Point(0,0), Point(90*math.cos(temp*9.4736842105),90*math.sin(temp*9.4736842105)))
-    aLine.draw(win)
+    if x%2 == 0:
+        aLine.setFill("red")
+    elif x%2 == 1:
+        aLine.setFill("black")
+    aLine.draw(wheel)
