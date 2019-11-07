53ee772d1d6e13173d2ee79854264089e0169d3a
Justin Gluska <justingluska@gmail.com>
2019-11-06 20:37:11 -0500

    spinBall()

--- roulette.py
+++ roulette.py
@@ -5,11 +5,12 @@
 ### IMPORT GRAPHICS AND MATH
 from graphics import *
 import math
-from random import randrange
+import time
+import random
 ### SET WHEEL WINDOW CONDITIONS
 wheel = GraphWin("Roulette Wheel", 750, 750)
 wheel.setCoords(-90, -90, 90, 90)
-wheel.setBackground("lightblue")
+wheel.setBackground("NavyBlue")
 #################################
 ### CREATE ROULETTE WHEEL
 def createWheel():
@@ -35,23 +36,49 @@ def createWheel():
     blockCircle.draw(wheel)
     blockCircle.setFill("maroon")
 createWheel()
+#############################################
 ### CREATE CONTROL WINDOW
+#############################################
 #otherWin = GraphWin("Bet Controls", 600, 300)
 #otherWin.setBackground("grey")
+#############################################
 ### DRAW POINTS ON WHEEL
+#############################################
 numberList = ["21","6","18","31","19","8","12","29","25","10","27","00","1","13","36","24","3","15","34","22","5","17","32","20","7","11","30","26","9","28","0","2","14","35","23","4","16","23"]
-def drawLabels():
+def createLabels():
     for x in range(38):
-        temp = (math.pi*x)/180
-        temp = temp - 5
-        temp2 = (math.pi*(x+1))/180
-        temp2 = temp - 5
+        temp = ((math.pi*x)/180)-5
         text = Text(Point((83*(math.cos(temp*9.4736842105))),(83*math.sin(temp*9.4736842105))), numberList[x])
         text.setSize(18)
         text.setTextColor("white")
         text.draw(wheel)
-drawLabels()
-
+#############################################
+##### CREATE BALL ON GREEN 0
+#############################################
+def spinBall():
+    ball = Circle(Point(63.4,5.5), 3)
+    ball.setFill("orange")
+    ball.draw(wheel)
+    spinCount = random.randrange(38,190)
+    print(spinCount)
+    k = 0
+    final = 0
+    for i in range(1,spinCount):
+        time.sleep(.05)
+        ball.move(-1*math.sin(0.165346981767014*i)*10.5,math.cos(0.165346981767014*i)*10.5)
+        k = i
+    print(numberList.index[k])
+    #for x in range(5):
+    #    y = math.pi*x
+    #    y = y / 180
+    #    ball.move(-1*((math.cos(.08728408*y))*73),(-1*(math.sin(.08728408*y)))*73)
+    #    time.sleep(.300)
+#############################################    
+##### PROGRAM RUN #####
+#############################################
+createWheel()
+createLabels()
+spinBall()
 slot0 = Text(Point(0,0), "make sure to eat your vegetables")
 slot0.setSize(22)
 slot0.setTextColor("orange")
